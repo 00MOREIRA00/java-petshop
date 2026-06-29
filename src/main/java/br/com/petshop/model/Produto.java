4221c1b8076cead2fae757305007b479248ed414
00MOREIRA00 <robertocorreamoreiraneto@gmail.com>
2026-06-29 19:56:58 -0300

    feat: adicionando classe de produto

--- src/main/java/br/com/petshop/model/Produto.java
+++ src/main/java/br/com/petshop/model/Produto.java
@@ -1,5 +1,65 @@
 package br.com.petshop.model;
 
-class Produto {
+public class Produto {
+    private Integer id;
+    private String nome;
+    private double preco;
+    private int estoque;
 
+    public Produto(){}
+
+    public Produto(String nome, double preco, int estoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    public Produto(Integer id, String nome, double preco, int estoque) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", estoque=" + estoque +
+                '}';
+    }
 }
