342331239a1e0f154b82089d91d0424d9e331d0e
00MOREIRA00 <robertocorreamoreiraneto@gmail.com>
2026-06-29 22:03:55 -0300

    feat: salvando atualizacao do banco e adicionando teste

--- src/main/java/br/com/petshop/Main.java
+++ src/main/java/br/com/petshop/Main.java
@@ -1,6 +1,7 @@
 package br.com.petshop;
 
 import br.com.petshop.model.Produto;
+import br.com.petshop.repository.ProdutoRepository;
 import br.com.petshop.service.ProdutoService;
 
 import java.util.Scanner;
@@ -10,6 +11,16 @@ public class Main {
         System.out.println("Bem-vindo ao PetShop!");
         System.out.println("-----------------------");
 
+        ProdutoRepository produtoRepository = new ProdutoRepository();
 
+        Produto produto = new Produto("Pato de Burracha", 15.90, 50);
+
+        produtoRepository.cadastrar(produto);
+
+        System.out.println("Produto cadastrado:");
+        System.out.println(produto);
+
+        System.out.println("Produtos no banco:");
+        produtoRepository.listarTodos().forEach(System.out::println);
     }
 }
